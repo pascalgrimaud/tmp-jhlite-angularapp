af9be31315c0a3e81caeecf661c74f18da50c5fc
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-04-03 18:57:55 +0200

    add angular jwt

--- src/main/webapp/app/common/primary/app/app.component.spec.ts
+++ src/main/webapp/app/common/primary/app/app.component.spec.ts
@@ -1,14 +1,44 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { FormBuilder } from '@angular/forms';
+import { of, Subject } from 'rxjs';
+import { LoginService } from './login/login.service';
+import { AccountService } from './auth/account.service';
+import { Account } from './model/account.model';
 
 import { AppComponent } from './app.component';
 
 describe('App Component', () => {
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let mockAccountService: AccountService;
+  let mockLoginService: LoginService;
+  const account: Account = {
+    activated: true,
+    authorities: [],
+    email: '',
+    firstName: null,
+    langKey: '',
+    lastName: null,
+    login: 'login',
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [HttpClientTestingModule, NgxWebstorageModule.forRoot()],
+      providers: [
+        FormBuilder,
+        AccountService,
+        {
+          provide: LoginService,
+          useValue: {
+            login: jest.fn(() => of({})),
+            logout: jest.fn(() => of({})),
+          },
+        },
+      ],
     })
       .overrideTemplate(AppComponent, '')
       .compileComponents();
@@ -17,15 +47,68 @@ describe('App Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
+    mockLoginService = TestBed.inject(LoginService);
+    mockAccountService = TestBed.inject(AccountService);
   });
 
   describe('ngOnInit', () => {
     it('should have appName', () => {
+      // GIVEN
+      const authenticationState = new Subject<Account | null>();
+      mockAccountService.getAuthenticationState = jest.fn(() => authenticationState.asObservable());
+
       // WHEN
       comp.ngOnInit();
 
       // THEN
       expect(comp.appName).toEqual('angularapp');
+      expect(mockAccountService.getAuthenticationState).toHaveBeenCalled();
+
+      // THEN
+      expect(comp.account).toBeNull();
+
+      // WHEN
+      authenticationState.next(account);
+
+      // THEN
+      expect(comp.account).toEqual(account);
+
+      // WHEN
+      authenticationState.next(null);
+
+      // THEN
+      expect(comp.account).toBeNull();
+    });
+  });
+
+  describe('login', () => {
+    it('should authenticate the user', () => {
+      // GIVEN
+      const credentials = {
+        username: 'admin',
+        password: 'admin',
+      };
+
+      comp.loginForm.patchValue({
+        username: 'admin',
+        password: 'admin',
+      });
+
+      // WHEN
+      comp.login();
+
+      // THEN
+      expect(mockLoginService.login).toHaveBeenCalledWith(credentials);
+    });
+  });
+
+  describe('logout', () => {
+    it('should logout the user', () => {
+      // WHEN
+      comp.logout();
+
+      // THEN
+      expect(mockLoginService.logout).toHaveBeenCalled();
     });
   });
 });
--- src/main/webapp/app/common/primary/app/app.component.ts
+++ src/main/webapp/app/common/primary/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { AccountService } from './auth/account.service';
+import { LoginService } from './login/login.service';
+import { Account } from './model/account.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   appName = '';
+
+  private readonly destroy$ = new Subject<void>();
+
+  account: Account | null = null;
+
+  loginForm = this.fb.group({
+    username: [null, [Validators.required]],
+    password: [null, [Validators.required]],
+  });
+
+  constructor(private accountService: AccountService, private loginService: LoginService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.appName = 'angularapp';
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  login(): void {
+    this.loginService
+      .login({
+        username: this.loginForm.get('username')!.value,
+        password: this.loginForm.get('password')!.value,
+      })
+      .subscribe();
+  }
+
+  logout(): void {
+    this.loginService.logout();
   }
 }
